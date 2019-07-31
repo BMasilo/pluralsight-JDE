6705d789da38a6d46a6d453a0f76cd17a2aea8a1
Masilo <Boikanyo.Masilo@yrbrands.com>
2019-07-31 16:50:44 +0200

    fix broken test

--- src/index.test.js
+++ src/index.test.js
@@ -13,8 +13,8 @@ describe('index.html', () => {
 		const index = fs.readFileSync('./src/index.html', 'utf-8');
 		jsdom.env(index, function (err, window) {
 			const h1 = window.document.getElementsByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal('hello World!');
-			(done);
+			expect(h1.innerHTML).to.equal('Hello World!');
+			done();
 			window.close();
 		})
 	})
